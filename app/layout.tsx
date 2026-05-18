7f0da078f66ca214752944cdd0d37706581773ef
imansession3-ux <imansession3@gmail.com>
2026-05-18 06:37:53 -0700

    Add RootLayout component with SpeedInsights

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
